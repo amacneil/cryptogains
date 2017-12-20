007ee93aa626a35e18248029324434a931ac36ab
AJ Slater <aj@slater.net>
2017-12-20 14:35:44 -0800

    bad extra character

--- src/cleandata.js
+++ src/cleandata.js
@@ -6,7 +6,7 @@ const { sequelize } = require('./sequelize');
 const { Transaction } = require('./models');
 const { assertNumEq } = require('./helpers');
 
-const BTC_PRICE_ENDPOINT = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=alltime&?format=csv';
+const BTC_PRICE_ENDPOINT = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=alltime&format=csv';
 const ETH_PRICE_ENDPOINT = 'https://www.etherchain.org/api/statistics/price';
 
 async function reconcileTransfers(amount, { fuzzyAmount }) {
