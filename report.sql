595cb7118f50a824fbe13dcc36e28582ea9150a3
Adrian Macneil <adrian@adrianmacneil.com>
2019-03-10 15:00:37 -0700

    Add report.sql

---
+++ report.sql
@@ -0,0 +1,16 @@
+-- use this sql to generate a report listing all disposals for a tax year
+-- for entry into form 8949
+select
+  currency,
+  case when min("acquiredAt")::date = max("acquiredAt")::date
+    then min("acquiredAt")::date::text
+    else 'VARIOUS' end as "date_acquired",
+  "disposedAt"::date as "date_sold",
+  term, count(id) as num_transactions, sum(amount) as amount,
+  sum("salePrice") as "sale_price",
+  sum("costBasis") as "cost_basis",
+  sum(gain) as gain
+from disposals
+where "disposedAt" between '2017-01-01' and '2018-01-01'
+group by "currency", "term", "date_sold"
+order by "currency", "date_sold"
